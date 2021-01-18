f8f40b4a031892808c4e94d768ac946104fdf359
siduck76 <siduckk76@protonmail.com>
2021-01-18 12:18:39 +0530

    cleaned

--- js_src/index.js
+++ js_src/index.js
@@ -4,7 +4,6 @@ const start = document.querySelector(".startBtn");
 // timer divided into two blocks : mins and secs
 var pomoMin = 25;
 var pomoSec = 00;
-var multipleClicks = 0;
 
 // displays the initial output for the timer
 pomoBox.innerText = `${pomoMin} : 0${pomoSec}`;
@@ -22,28 +21,26 @@ const timerSound = () => {
   audio.play();
 };
 
+const checkTimer = (temp) => {
+  if (pomoMin == 0) {
+    if (pomoSec == 0) {
+      clearInterval(temp), (pomoSec = 0), timerSound();
+      return;
+    }
+  }
+};
+
 const startPomodoro = () => {
-  (pomoSec = 60), (pomoMin = 24);
+  pomoSec = 60 , pomoMin = 24;
+
   const timer = setInterval(function () {
-    // check timer done or nu
-    const checkk = () => {
-      if (pomoMin == 0) {
-        if (pomoSec == 0) {
-          clearInterval(timer);
-          pomoSec = 0;
-          timerSound();
-        }
-      }
-    };
     pomoSec--;
-    pomoSec == 0
-      ? (boxText(pomoMin, pomoSec), checkk(), (pomoSec = 60), pomoMin--)
-      : boxText(pomoMin, pomoSec);
+    pomoSec == 0 ?
+      ( boxText(pomoMin, pomoSec) , checkTimer(timer), pomoSec = 60, pomoMin-- ) :
+        boxText(pomoMin, pomoSec);
   }, 1000);
 };
 
 // restart btn
-start.addEventListener("click", () => {
-  multipleClicks++;
-  multipleClicks >= 2 ? startPomodoro() : startPomodoro();
-});
+start.addEventListener("click", () => startPomodoro());
+
--- js_src/modules/sounds.js
+++
@@ -1,6 +0,0 @@
-const timerSound = () => {
-  var audio = new Audio("./assets/sounds/cocGame.mp3");
-  audio.play();
-};
-
-export {timerSound} ;
