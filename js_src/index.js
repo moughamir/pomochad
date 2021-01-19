99ba8d0f28e2223f24233b2e3b68ac8fb4c2971b
siduck76 <siduckk76@protonmail.com>
2021-01-19 10:22:34 +0530

    pause btn

--- js_src/index.js
+++ js_src/index.js
@@ -1,13 +1,17 @@
 const pomoBox = document.querySelector(".pomoBox"); // inner circle
 const start = document.querySelector(".fa-redo");
+const pause = document.querySelector(".fa-stop");
+let isPaused = false;
 
 // timer divided into two blocks : mins and secs
-var pomoMin = 25;
-var pomoSec = 00;
+let pomoMin = 25,
+  pomoSec = 00;
 
-pomoBox.innerText = `${pomoMin} : 0${pomoSec}`;  // displays the initial output for the timer 
+let saveMin, saveSec;
 
-// displays timer's text 
+pomoBox.innerText = `${pomoMin} : 0${pomoSec}`; // displays the initial output for the timer
+
+// displays timer's text
 const boxText = (min, sec) => {
   if (min < 10) pomoBox.innerText = `0${min} : ${sec}`;
   else if (sec < 10) pomoBox.innerText = `${min} : 0${sec}`;
@@ -28,20 +32,33 @@ const checkTimer = (temp) => {
   }
 };
 
-let timer  ; 
+let timer;
 
 const startPomodoro = () => {
-  (pomoSec = 60), (pomoMin = 24);
+  isPaused == false
+    ? ((pomoSec = 60), (pomoMin = 24))
+    : ((pomoMin = saveMin), (pomoSec = saveSec));
 
   timer = setInterval(function () {
-  pomoSec--;
-  pomoSec == 0
-    ? (boxText(pomoMin, pomoSec), checkTimer(timer), (pomoSec = 60), pomoMin--)
-    : boxText(pomoMin, pomoSec);
-}, 1000);
+    pomoSec--;
+    pomoSec == 0
+      ? (boxText(pomoMin, pomoSec),
+        checkTimer(timer),
+        (pomoSec = 60),
+        pomoMin--)
+      : boxText(pomoMin, pomoSec);
+  }, 1000);
 };
 
 // restart timer
 start.addEventListener("click", () => {
-  clearInterval(timer) , startPomodoro() ;
+  clearInterval(timer), startPomodoro();
+});
+
+pause.addEventListener("click", () => {
+  saveMin = pomoMin;
+  saveSec = pomoSec;
+
+  isPaused = true;
+  clearInterval(timer), startPomodoro();
 });
