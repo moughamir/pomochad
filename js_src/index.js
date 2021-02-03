fe956d6a3cf3887f95bdb7f7ce662d581117f37a
siduck76 <siduckk76@protonmail.com>
2021-02-03 20:14:49 +0530

    test

--- js_src/index.js
+++ js_src/index.js
@@ -57,7 +57,7 @@ export const startPomodoro = () => {
   addShakeAnimation();
 
   currentClick == "pause" ? (pomoTime = saveMin) : (pomoTime = totalTime * 60);
-  timer = setInterval(updatePomodoro, 1000);
+  timer = setInterval(updatePomodoro,0);
 };
 
 toggleTheme();
