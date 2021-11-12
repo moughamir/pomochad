b5c39ca672ed7f444fa507a1711f94786fbb0012
siduck <siduck@tutanota.com>
2021-11-12 12:00:18 +0530

    use relative paths

--- src/components/timer.jsx
+++ src/components/timer.jsx
@@ -7,10 +7,8 @@ import "../css/timer.css";
 
 import { numToText, playSound, setProgressValue } from "../utils";
 
-let tickSound =
-    "https://github.com/siduck/pomoReward/blob/preact-rewrite/assets/tick.mp3",
-  timerSound =
-    "https://github.com/siduck/pomoReward/blob/preact-rewrite/assets/pikachu.mp3";
+let tickSound = "../../assets/tick.mp3",
+  timerSound = "../../assets/pikachu.mp3";
 
 export let timer, pausedTime, currentClick;
 export let totalTime = 25,
