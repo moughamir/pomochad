624b5108791cb13c9d8e11c53f73be8d340e04c2
siduck <siduckk76@protonmail.com>
2021-12-14 17:44:51 +0530

    btn for adding projects

---
+++ src/components/todos/sidebar.jsx
@@ -0,0 +1,96 @@
+import { Fire, FireSimple, Notepad, PlusCircle, XCircle } from "phosphor-react";
+import { store, view } from "@risingstack/react-easy-state";
+
+export const projects = store({
+  showInputBox: false,
+  list: [],
+});
+
+function showInput(tmp) {
+  projects.showInputBox = (tmp) ? true : false;
+}
+
+// input field for adding new projects
+function ProjectInput() {
+  let tmpProject;
+
+  function addProject() {
+    projects.list.push(tmpProject);
+    showInput(false);
+  }
+
+  return (
+    <div className="projectInput">
+      <input
+        type="text"
+        placeholder="project name"
+        onChange={(e) => tmpProject = e.target.value}
+      />
+
+      <div className="projectInputBtns">
+        <button onClick={() => addProject()}>
+          save
+        </button>
+
+        <XCircle
+          className="closeProjectInput"
+          size={20}
+          weight="fill"
+          onClick={() => showInput(false)}
+        />
+      </div>
+    </div>
+  );
+}
+
+function DefaultProjects() {
+  return (
+    <div className="DefaultProjects">
+      <button className="todoProject">
+        <Notepad size={20} style={{ color: "var(--blue)" }} />Today
+      </button>
+
+      <button className="todoProject">
+        <Fire size={20} style={{ color: "var(--red)" }} />Important
+      </button>
+    </div>
+  );
+}
+
+function Project(props) {
+  return (
+    <button className="todoProject">
+      <FireSimple
+        size={20}
+      />
+      {props.name}
+    </button>
+  );
+}
+
+function ProjectLists() {
+  return (
+    <div className="projectLists">
+      {projects.list.map((item) => <Project name={item} />)}
+    </div>
+  );
+}
+
+export default view(() => (
+  <div className="sidebar">
+    <div className="projects">
+      <DefaultProjects />
+      <ProjectLists />
+    </div>
+
+    <div style={{ position: "relative" }}>
+      {projects.showInputBox && (
+        <ProjectInput closeInput={() => showInput(false)} />
+      )}
+
+      <button className="newProjectBtn" onClick={() => showInput(true)}>
+        <PlusCircle size={24} /> new project
+      </button>
+    </div>
+  </div>
+));
--- src/components/todos/todos.jsx
+++ src/components/todos/todos.jsx
@@ -1,52 +1,5 @@
-import { useState } from "preact/hooks";
-
 import "./todos.css";
-import { Fire, Notepad, PlusCircle, XCircle } from "phosphor-react";
-
-function ProjectInput(props) {
-  return (
-    <div className="projectInput">
-      <input type="text" placeholder="project name" />
-
-      <div className="projectInputBtns">
-        <button>save</button>
-
-        <XCircle
-          className="closeProjectInput"
-          size={20}
-          weight="fill"
-          onClick={props.closeInput}
-        />
-      </div>
-    </div>
-  );
-}
-
-function Sidebar() {
-  let [showInput, setInput] = useState(false);
-
-  return (
-    <div className="sidebar">
-      <div className="todoProjects">
-        <button className="todoProject">
-          <Notepad size={25} style={{ color: "var(--blue)" }} /> Today
-        </button>
-
-        <button className="todoProject">
-          <Fire size={25} style={{ color: "var(--red)" }} /> Important
-        </button>
-      </div>
-
-      <div style={{ position: "relative" }}>
-        {showInput && <ProjectInput closeInput={() => setInput(false)} />}
-
-        <button className="newProjectBtn" onClick={() => setInput(true)}>
-          <PlusCircle size={24} /> new project
-        </button>
-      </div>
-    </div>
-  );
-}
+import Sidebar from "./sidebar";
 
 function TodoList() {
   return (
