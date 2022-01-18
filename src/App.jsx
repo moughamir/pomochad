5dfe55e5166446fb1a7a33962c80c3ce570eabde
siduck <siduckk76@protonmail.com>
2022-01-18 16:30:45 +0530

    refactor!

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,18 @@
 import "./css/style.css";
+import "./css/themes.css";
 import "./css/animations.css";
 
-import { Route } from "wouter-preact";
+// components
 import Navbar from "./components/navbar";
 import Timer from "./components/timer";
 import Settings from "./components/settings";
-import TodoPage from "./components/todoPage.jsx"
+import TodoPage from "./components/todoPage.jsx";
 
-export default () => {
+import { switchTheme } from "./utils";
+import { render } from "preact";
+import { Route } from "wouter-preact";
+
+function App() {
   return (
     <>
       <Navbar />
@@ -25,4 +30,14 @@ export default () => {
       </Route>
     </>
   );
-};
+}
+
+// set theme
+(localStorage.theme == "undefined" || !localStorage.theme)
+  ? switchTheme("light")
+  : switchTheme(localStorage.theme);
+
+render(
+  <App />,
+  document.getElementById("root"),
+);
